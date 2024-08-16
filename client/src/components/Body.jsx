435a8261e17a514670c36a7c7175a89c26744742
Basit-Perampalli <apeerampalli@gmail.com>
2024-08-16 22:09:03 +0530

    started with frontend and added mui

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Pagination,Stack,Box } from '@mui/material'
+const Body = () => {
+  return (
+    <Stack justifyContent="space-between" alignItems="center" flexGrow={true}>
+        <Box>
+            Hello
+        </Box>
+    </Stack>
+  )
+}
+
+export default Body
